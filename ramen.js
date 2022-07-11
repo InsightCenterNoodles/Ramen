d4b676b40bb2d7b21cd21e366c8fffd830a5dd69
Nicholas Brunhart-Lupo <nicholasbl@icloud.com>
2022-07-11 15:29:05 -0600

    Basic functions

--- ramen.js
+++ ramen.js
@@ -1,12 +1,5 @@
 (function(global, undefined) { "use strict";
 
-/*
-delegates can have 
-on_create(state)
-on_delete(state)
-on_update(state)
-*/
-
 const client_delegate_maker = {
     method : null,
     signal : null,
@@ -31,32 +24,39 @@ class DelegateList {
         this.client_delegate = null
     }
 
+    get(slot) {
+        if (Array.isArray(slot)) {
+            return this.slot_list[slot[0]]    
+        }
+        return this.slot_list[slot]
+    }
+
     reset() { 
         this.slot_list = {}
     }
 
-    on_create_msg(m) {
+    on_create_msg(client, m) {
         let slot = m.id[0]
         console.log(this.name, "create", slot)
         this.slot_list[slot] = m
         if (this.client_delegate.on_create){
-            this.client_delegate.on_create(m)
+            this.client_delegate.on_create(client, m)
         }
     }
-    on_delete_msg(m) {
+    on_delete_msg(client, m) {
         let slot = m.id[0]
         console.log(this.name, "delete", slot)
         if (this.client_delegate.on_delete){
-            this.client_delegate.on_delete(this.slot_list[slot])
+            this.client_delegate.on_delete(client, this.slot_list[slot])
         }
         delete this.slot_list[slot]
     }
-    on_update_msg(m) {
+    on_update_msg(client, m) {
         console.log(this.name, "update", slot)
         let slot = m.id[0]
         update_keys(this.slot_list[slot], m)
         if (this.client_delegate.on_update){
-            this.client_delegate.on_update(m)
+            this.client_delegate.on_update(client, m)
         }
     }
 }
@@ -69,30 +69,38 @@ function update_keys(old_o, new_o) {
     }
 }
 
-class State {
-    constructor(delegate_makers) {
-        this.makers = delegate_makers
+class Client {
+    constructor(url, delegate_makers) {
+        this.socket = new WebSocket(url)
+        this.socket.binaryType = "arraybuffer"
 
-        console.log(this.makers)
+        this.socket.onopen = this.on_socket_open.bind(this)
+        this.socket.onmessage = this.on_socket_message.bind(this)
+        this.socket.onclose = this.on_socket_close.bind(this)
+        this.socket.onerror = this.on_socket_error.bind(this)   
+
+        this.delegate_makers = delegate_makers
+
+        console.log(this.delegate_makers)
 
         this.delegate_lists = {}
         this.delegate_handlers = new Map()
 
         this.document = {}
 
-        this.add_delegate("method", 0, 1)
-        this.add_delegate("signal", 2, 3)
-        this.add_delegate("entity", 4, 6, 5)
-        this.add_delegate("plot", 7, 9, 8)
-        this.add_delegate("buffer", 10, 11)
-        this.add_delegate("bufferview", 12, 13)
-        this.add_delegate("material", 14, 16, 15)
-        this.add_delegate("image", 17, 18)
-        this.add_delegate("texture", 19, 20)
-        this.add_delegate("sampler", 21, 22)
-        this.add_delegate("light", 23, 25, 24)
-        this.add_delegate("geometry", 26, 27)
-        this.add_delegate("table", 28, 30, 29)
+        this.add_delegate_list("method", 0, 1)
+        this.add_delegate_list("signal", 2, 3)
+        this.add_delegate_list("entity", 4, 6, 5)
+        this.add_delegate_list("plot", 7, 9, 8)
+        this.add_delegate_list("buffer", 10, 11)
+        this.add_delegate_list("bufferview", 12, 13)
+        this.add_delegate_list("material", 14, 16, 15)
+        this.add_delegate_list("image", 17, 18)
+        this.add_delegate_list("texture", 19, 20)
+        this.add_delegate_list("sampler", 21, 22)
+        this.add_delegate_list("light", 23, 25, 24)
+        this.add_delegate_list("geometry", 26, 27)
+        this.add_delegate_list("table", 28, 30, 29)
 
         this.delegate_handlers.set(31, this.on_document_update.bind(this))
         this.delegate_handlers.set(32, this.on_document_reset.bind(this))
@@ -100,26 +108,58 @@ class State {
         this.delegate_handlers.set(34, this.on_method_reply.bind(this))
     }
 
-    on_document_update(m) {
-        update_keys(this.document, m)
-        console.log("here", this.document)
+    // SOCKET HANDLERS
+
+    on_socket_open(e) {
+        let intro = [0,{
+            client_name: "Web Browser"
+        }]
+
+        this.socket.send(CBOR.encode(intro))
     }
 
-    on_document_reset() {
+    on_socket_message(e) {
+        var decoded = CBOR.decode(e.data);   
+        
+        for (let i = 0, len = decoded.length; i < len; i += 2) {
+            this.handle_decoded_message(decoded[i], decoded[i+1])
+        }
+    }
+
+    on_socket_close(e) {
+        if (e.wasClean) {
+            console.log(`[websocket][close] Connection closed: ${e.reason}`)
+        } else {
+            console.log(`[websocket][close] Connection killed`)
+        }
+        
+    }
+
+    on_socket_error(e) {
+        console.log(`[websocket][error] ${e}`)
+    }
+
+    // CLIENT HANDLERS
+
+    on_document_update(client, m) {
+        update_keys(this.document, m)
+    }
+
+    on_document_reset(client, m) {
         for (value in this.delegate_lists){
             value.reset()
         }
     }
 
-    on_signal_invoke(m) {
+    on_signal_invoke(client, m) {
 
     }
 
-    on_method_reply(m) {
+    on_method_reply(client, m) {
 
     }
 
-    add_delegate(name, create_mid, delete_mid, update_mid) {
+    add_delegate_list(name, create_mid, delete_mid, update_mid) {
         console.log("Adding delegate list", name)
         let dl = new DelegateList(name);
 
@@ -130,7 +170,7 @@ class State {
         }
 
         try {
-            update_keys(client_delegate_template, this.makers[name])
+            update_keys(client_delegate_template, this.delegate_makers[name])
             console.log(`Installing delegates for ${name}`)
         } catch (error) {
             console.log(`No delegates for ${name}`)
@@ -146,9 +186,10 @@ class State {
         if (update_mid !== undefined) { 
             this.delegate_handlers.set(update_mid, dl.on_update_msg.bind(dl))
         }
+        this[name+"_list"] = dl
     }
 
-    handle_message(mid, content) {
+    handle_decoded_message(mid, content) {
         console.log("Handling", mid, content)
 
         if (!this.delegate_handlers.has(mid)) {
@@ -156,50 +197,7 @@ class State {
             return;
         }
 
-        this.delegate_handlers.get(mid)(content)
-    }
-}
-
-class Client {
-    constructor(url, delegate_makers) {
-        this.state = new State(delegate_makers)
-
-        this.socket = new WebSocket(url)
-        this.socket.binaryType = "arraybuffer"
-
-        this.socket.onopen = this.on_open.bind(this)
-        this.socket.onmessage = this.on_message.bind(this)
-        this.socket.onclose = this.on_close.bind(this)
-        this.socket.onerror = this.on_socket_error.bind(this)   
-    }
-
-    on_open(e) {
-        let intro = [0,{
-            client_name: "Web Browser"
-        }]
-
-        this.socket.send(CBOR.encode(intro))
-    }
-
-    on_message(e) {
-        var decoded = CBOR.decode(e.data);   
-        
-        for (let i = 0, len = decoded.length; i < len; i += 2) {
-            this.state.handle_message(decoded[i], decoded[i+1])
-        }
-    }
-
-    on_close(e) {
-        if (e.wasClean) {
-            console.log(`[websocket][close] Connection closed: ${e.reason}`)
-        } else {
-            console.log(`[websocket][close] Connection killed`)
-        }
-        
-    }
-
-    on_socket_error(e) {
-        console.log(`[websocket][error] ${e.message}`)
+        this.delegate_handlers.get(mid)(this, content)
     }
 }
 
--- test_client.js
+++ test_client.js
@@ -1,59 +1,474 @@
 
-function on_entity_create(state) {
-    console.log("Hello!")
-
-
-    if (!('dataready' in state)) {
-        
-    }
-}
-
-function is_buffer_ready()
-
-function mark_buffer_ready(state, bytes) {
-    state.dataready = true
-    state.bytes = bytes
-}
-
-function on_buffer_create(state) {    
-    console.log(state)
-
-    if ("inline_bytes" in state) {
-        mark_buffer_ready(state, state.inline_bytes)
-    } else {
-
-    }
-}
-
-client = NOO.connect("ws://localhost:50000",
-    {
-        entity : { on_create : on_entity_create },
-        buffer : { on_create : on_buffer_create },
-    }
-)
-
+// Set up render window
 width = 720 // window.innerWidth
 height = 512
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( width, height );
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.listenToKeyEvents(window); // optional
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.minDistance = .1;
+controls.maxDistance = 500;
+controls.maxPolarAngle = Math.PI / 2;
+
+const format_to_bytesize = {
+    "U8": 1,
+    "U8VEC4": 1 * 4,
+    "U16": 2,
+    "U16VEC2": 2 * 2,
+    "U32": 4,
+    "VEC2": 4 * 2,
+    "VEC3": 4 * 3,
+    "VEC4": 4 * 4,
+    "MAT3": 4 * 3 * 3,
+    "MAT4": 4 * 4 * 4
+}
+
+const format_to_simdcount = {
+    "U8": 1,
+    "U8VEC4": 4,
+    "U16": 1,
+    "U16VEC2": 2,
+    "U32": 1,
+    "VEC2": 2,
+    "VEC3": 3,
+    "VEC4": 4,
+    "MAT3": 3 * 3,
+    "MAT4": 4 * 4
+}
+
+function bytes_to_interleaved_buffer(format, bytes, offset, stride, vertex_count) {
+    console.log(`Format to typed array Format: ${format}, Bytes len ${bytes.byteLength}, Offset ${offset}, Stride ${stride}, Vcount ${vertex_count}`)
+    let arr = null;
+    let interleaved_count = 0;
+    const simd_byte_size = format_to_bytesize[format]
+
+    switch (format) {
+        case "U8":
+        case "U8VEC4":
+            arr = new Uint8Array(bytes, offset, stride * vertex_count)
+            interleaved_count = 1;
+            break;
+
+        case "U16":
+        case "U16VEC2":
+            arr = new Uint16Array(bytes, offset, stride / 2 * vertex_count)
+            interleaved_count = 2;
+            break;
+
+        case "U32":
+            arr = new Uint32Array(bytes, offset, stride / 4 * vertex_count)
+            interleaved_count = 4;
+            break;
+        case "VEC2":
+        case "VEC3":
+        case "VEC4":
+        case "MAT3":
+        case "MAT4":
+            arr = new Float32Array(bytes, offset, stride / 4 * vertex_count)
+            interleaved_count = 4;
+            break;
+        default:
+            throw "Invalid format"
+    }
+
+    interleaved_count = stride / interleaved_count
+
+    return new THREE.InterleavedBuffer(arr, interleaved_count)
+}
+
+function get_or_default(o, prop, def) {
+    if (prop in o) return o[prop]
+    return def
+}
+
+const semantic_translate = {
+    "POSITION": "position",
+    "NORMAL": "normal",
+    "COLOR": "color"
+}
+
+function view_to_attribute(patch, attrib, three_geometry, on_done) {
+    const sname = semantic_translate[attrib.semantic]
+
+    let view = client.bufferview_list.get(attrib.view)
+    let buffer = client.buffer_list.get(view.source_buffer)
+    buffer.byte_promise.then(function (bytes) {
+        console.log("Setting up attribute from view", attrib, view)
+        console.log(`View of ${bytes.byteLength} bytes`)
+        const offset = get_or_default(view, 'offset', 0) +
+            get_or_default(attrib, 'offset', 0)
+
+        const stride = function () {
+            let f = format_to_bytesize[attrib.format]
+            let s = get_or_default(attrib, 'stride', 0)
+            if (s < f) s = f
+            return s
+        }();
+
+        const length = stride * patch.vertex_count
+
+        byte_view = new ArrayBuffer()
+
+        const normalized = get_or_default(attrib, 'normalized', false)
+
+        console.log("Resolved view parts", offset, length, stride)
+
+        let interleaved_buffer = bytes_to_interleaved_buffer(
+            attrib.format,
+            bytes,
+            offset, stride, patch.vertex_count
+        );
+
+        console.log("Interleaved attrib", interleaved_buffer)
+
+        console.assert(interleaved_buffer.array.length > 0)
+
+        let interleaved_attribute = new THREE.InterleavedBufferAttribute(
+            interleaved_buffer,
+            format_to_simdcount[attrib.format],
+            //offset,
+            0,
+            normalized
+        )
+
+        three_geometry.setAttribute(sname, interleaved_attribute)
+
+        console.log("Added attribute", interleaved_attribute, "to", three_geometry)
+
+        on_done()
+
+    });
+}
+
+function view_to_index(patch, three_geometry, on_done) {
+    let index_info = p.indicies
+    console.log("Adding index", index_info)
+
+    let indicies = p.indicies
+    let view = client.bufferview_list.get(indicies.view)
+    let buffer = client.buffer_list.get(view.source_buffer)
+    buffer.byte_promise.then(function (bytes) {
+        console.log("Setting up index from view", view)
+        console.log(`View of ${bytes.byteLength} bytes`)
+        const offset = get_or_default(view, 'offset', 0) +
+            get_or_default(indicies, 'offset', 0)
+
+        let format_byte_size = format_to_bytesize[indicies.format]
+
+        const count = index_info.count
+
+        const stride = function () {
+            let s = get_or_default(indicies, 'stride', 0)
+            if (s < format_byte_size) s = format_byte_size
+            return s
+        }();
+
+        if (stride != format_byte_size) {
+            // do a copy
+            throw "TODO: Do a copy here"
+        }
+
+        let typed_arr = [];
+
+        switch (indicies.format) {
+            case "U8":
+                typed_arr = new Uint8Array(bytes, offset, count)
+                break;
+            case "U16":
+                typed_arr = new Uint16Array(bytes, offset, count)
+                break;
+            case "U32":
+                typed_arr = new Uint32Array(bytes, offset, count)
+                break;
+            default:
+                throw "Invalid format"
+        }
+
+        console.log(typed_arr)
+
+        three_geometry.setIndex(Array.from(typed_arr))
+
+        console.log(`Added index ${typed_arr.length} to`, three_geometry)
+
+        on_done()
+    });
+}
+
+function noo_color_convert(noo_col) {
+    console.log("COLOR", noo_col)
+    return new THREE.Color(noo_col[0], noo_col[1], noo_col[2])
+}
+
+function make_render_rep(client, parent, render_rep) {
+
+    let m = client.geometry_list.get(render_rep.mesh);
+
+    m.pending_sub_meshs.then(function (value) {
+        console.log("Creating mesh group")
+        let group = new THREE.Group()
+
+
+        for (i of value) {
+            console.log("Adding sub object...", i)
+
+            const noo_mat = client.material_list.get(i.noo_patch.material)
+
+            let mat;
+
+            switch (i.noo_patch.type) {
+                case "POINTS":
+                    mat = noo_mat.three_points
+                    break;
+                case "LINES":
+                case "LINE_LOOP":
+                case "LINE_STRIP":
+                    mat = noo_mat.three_lines
+                    break;
+                case "TRIANGLES":
+                case "TRIANGLE_STRIP":
+                    mat = noo_mat.three_tris
+                    break;
+            }
+
+            let o
+
+            switch (i.noo_patch.type) {
+                case "POINTS":
+                    o = new THREE.Points(i, mat)
+                    break;
+                case "LINES":
+                    o = new THREE.LineSegments(i, mat)
+                    break;
+                case "LINE_LOOP":
+                    o = new THREE.LineLoop(i, mat)
+                    break;
+                case "LINE_STRIP":
+                    o = new THREE.Line(i, mat)
+                    break;
+                case "TRIANGLES":
+                    o = new THREE.Mesh(i, mat)
+                    break;
+                case "TRIANGLE_STRIP":
+                    throw "Not yet implemented"
+                    break;
+            }
+
+            group.add(o)
+        }
+
+        console.log("Adding group to parent", parent)
+
+        parent.add(group)
+
+    });
+}
+
+function on_entity_create(client, state) {
+    console.log("New 3js entity")
+
+    let e = new THREE.Object3D()
+    state.three_entity = e
+
+    e.name = state.name
+
+    if (state.parent) {
+        client.entity_list.get(state.parent).three_entity.add(e)
+    } else {
+        scene.add(e)
+    }
+
+    if (state.transform) {
+        e.matrixAutoUpdate = false
+        e.matrixWorldNeedsUpdate = true
+        let m = new THREE.Matrix4()
+        m.set(...state.transform)
+        m.transpose();
+        console.log("UPDATE TF", e.uuid, state.transform, m);
+        e.matrix = m
+    }
+
+    if (state.render_rep) {
+        state.concrete_rep = make_render_rep(client, e, state.render_rep)
+    }
+
+}
+
+function on_buffer_create(client, state) {
+    console.log(state)
+
+    state.byte_promise = new Promise(function (resolve, reject) {
+        if ("inline_bytes" in state) {
+            //console.log(state.inline_bytes.buffer)
+            // plain .buffer might not refer to the right thing..
+
+            const arr = state.inline_bytes
+
+            const view = arr.buffer.slice(arr.byteOffset, arr.byteLength + arr.byteOffset)
+
+            resolve(view);
+            return;
+        }
+
+        let req = new XMLHttpRequest();
+        req.open("GET", state.uri_bytes)
+        req.responseType = "arraybuffer";
+        req.onload = function () {
+            if (req.status == 200) {
+                console.log("Download completed")
+                console.log(req.response)
+                resolve(req.response)
+            } else {
+                reject("Buffer not found")
+            }
+        }
+        req.send();
+    });
+}
+
+function on_mesh_create(client, state) {
+    state.pending_sub_meshs = new Promise(function (resolve) {
+
+        let arr = []
+
+        console.log("Mesh", state.patches)
+
+        let to_go = state.patches.length
+
+        let dec_func = function () {
+            to_go -= 1
+
+            if (to_go == 0) {
+                resolve(arr)
+            }
+        }
+
+        for (p of state.patches) {
+            let g = new THREE.BufferGeometry();
+
+            g.noo_patch = p
+
+            console.log("Patch", p)
+            for (a of p.attributes) {
+
+                view_to_attribute(p, a, g, dec_func)
+
+                console.log("Attribute", a.semantic)
+            }
+
+            if ("indicies" in p) {
+                to_go += 1
+                view_to_index(p, g, dec_func)
+            }
+
+            console.log("Adding sub mesh...")
+            arr.push(g)
+        }
+    });
+
+}
+
+function on_mesh_delete(client, state) {
+    state.pending_sub_meshs.then(
+        function (value) {
+            for (i of value) {
+                i.dispose()
+            }
+        }
+    )
+}
+
+function on_material_create(client, state) {
+    console.log("NEW MATERIAL", state)
+    const noo_pbr = state.pbr_info
+
+    const noo_base_col = noo_color_convert(noo_pbr.base_color)
+
+    state.three_points = new THREE.PointsMaterial({ color: noo_base_col })
+    state.three_lines = new THREE.LineBasicMaterial({ color: noo_base_col })
+    state.three_tris = new THREE.MeshPhysicalMaterial({
+        color: noo_base_col,
+        metalness: noo_pbr.metallic,
+        roughness: noo_pbr.roughness,
+    })
+
+    // state.three_tris = new THREE.MeshBasicMaterial({
+    //     color: 0x00ff00, side: THREE.DoubleSide
+    // });
+}
+
+function on_material_delete(client, state) {
+    state.three_points.dispose()
+    state.three_lines.dispose()
+    state.three_tris.dispose()
+}
+
+function start_connect() {
+    let url;
+    try {
+        url = new URL(document.getElementById("server_url").value);
+    } catch {
+        url = new URL("ws://localhost:50000");
+    }
+
+    console.log(`Starting connection to ${url}`)
+
+    client = NOO.connect(url.toString(),
+        {
+            entity: { on_create: on_entity_create },
+            buffer: { on_create: on_buffer_create },
+            //       bufferview : { on_create : on_bufferview_create },
+            geometry: {
+                on_create: on_mesh_create,
+                on_delete: on_mesh_delete
+            },
+            material: {
+                on_create: on_material_create,
+                on_delete: on_material_delete
+            }
+        }
+    )
+}
+
+
+if (false) {
+    const geometry = new THREE.BoxGeometry(.1, .1, .1);
+    const material = new THREE.MeshPhysicalMaterial({ color: 0x00ff00 });
+
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+}
+
+{
+    let light_object = new THREE.DirectionalLight(0xffffff, 2.0)
+
+    light_object.position.set(1, 1, 1)
+    light_object.castShadow = true
+
+    scene.add(light_object)
+
+    let amb_light_object = new THREE.AmbientLight(0x101010)
+    scene.add(amb_light_object)
+}
 
 camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+
+    controls.update();
+
+    renderer.render(scene, camera);
 }
 
 animate();
